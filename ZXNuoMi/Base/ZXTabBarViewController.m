1e47ed14b471750c501c581fd12ea71e7a1cd850
zhangxin0915 <macmini@ZW.local>
2016-04-19 13:42:23 +0800

    基础框架， 设置tabbarItem 的字体颜色 ， 选中图片。

--- ZXNuoMi/Base/ZXTabBarViewController.m
+++ ZXNuoMi/Base/ZXTabBarViewController.m
@@ -19,9 +19,27 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
 
+    [self setupTabBarItem];
+}
+
+-(void)setupTabBarItem
+{
+    UITabBar *tabbar = self.tabBar;
+    UITabBarItem *item0 = [tabbar.items objectAtIndex:0];
+    UITabBarItem *item1 = [tabbar.items objectAtIndex:1];
+    UITabBarItem *item2 = [tabbar.items objectAtIndex:2];
+    UITabBarItem *item3 = [tabbar.items objectAtIndex:3];
+    
+    item0.selectedImage = [[UIImage imageNamed:@"icon_tab_shouye_highlight"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+    item0.image = [[UIImage imageNamed:@"icon_tab_shouye_normal"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+    item1.selectedImage = [[UIImage imageNamed:@"icon_tab_fujin_highlight"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+    item1.image = [[UIImage imageNamed:@"icon_tab_fujin_normal"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+    item2.selectedImage = [[UIImage imageNamed:@"tab_icon_selection_highlight"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+    item2.image = [[UIImage imageNamed:@"tab_icon_selection_normal"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+    item3.selectedImage = [[UIImage imageNamed:@"icon_tab_wode_highlight"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+    item3.image = [[UIImage imageNamed:@"icon_tab_wode_normal"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
     // 改变UITabBarItem字体颜色
     [[UITabBarItem appearance]setTitleTextAttributes:@{UITextAttributeTextColor:navigationBarColor} forState:UIControlStateSelected];
-    
 }
 
 
